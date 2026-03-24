68b341bda7eddeaf4d2ff8894b914fefd7dd11bc
keshav9480 <keshav9480@gmail.com>
2026-03-25 04:47:37 +0530

    added support for the arm

--- phase1/Threads.cpp
+++ phase1/Threads.cpp
@@ -12,23 +12,41 @@ This file contains the implementation of async using threads.
 #include <mutex>
 #include <vector>
 
+#if defined(__APPLE__)
+    #include <mach/mach.h>
+    #include <mach/thread_info.h>
+#endif
+
 
 
 int get_cpu_number()
 {
-    #ifdef __linux__
+    #if defined(__linux__)
         #define _GNU_SOURCE
         #include <sched.h>
         return sched_getcpu();
+
     #elif defined(_WIN32)
         #include <windows.h>
-        return GETCurrentProcessorNumber();
+        return GetCurrentProcessorNumber();
+
+    #elif defined(__APPLE__)
+        thread_identifier_info_data_t t_id;
+        mach_msg_type_number_t count = THREAD_IDENTIFIER_INFO_COUNT;
+
+        if (thread_info(mach_thread_self(), THREAD_IDENTIFIER_INFO, (thread_info_t)&t_id, &count) != KERN_SUCCESS) 
+        {
+            return -1;
+        }
+
+        return (int)t_id.thread_id;
     #else
         return -1;
     #endif
 }
 
 int main() {
+
     unsigned num_cpus = std::thread::hardware_concurrency();
     std::cout<<"Num of cpus "<<num_cpus<<std::endl;
     
@@ -39,12 +57,11 @@ int main() {
     {
         // capture by reference
         threads[i] = std::thread([&mtx, i](){
-            {
+        {
             std::lock_guard<std::mutex> lock(mtx);
             std::cout<<"Thread: " << i <<" is running on CPU: "<< get_cpu_number() <<std::endl;
             std::this_thread::sleep_for(std::chrono::microseconds(300));
-            }
-        });   
+        }});
     }
 
     for (auto& t : threads){
